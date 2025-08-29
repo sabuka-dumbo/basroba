931a1c7c53880da4a4bfab2c91f4dc076dd564f2
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-29 10:01:07 +0400

    Changes

--- basroba/app/urls.py
+++ basroba/app/urls.py
@@ -2,5 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("products/", views.products, name="products"),
+    path("product/", views.product, name="product"),
+    path("cart/", views.cart, name="cart"),
+    path("profile/", views.profile, name="profile"),
+    path("favorites/", views.favorites, name="favorites"),
 ]
--- basroba/app/views.py
+++ basroba/app/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def products(request):
+    return render(request, "products.html")
+
+def product(request):
+    return render(request, "product.html")
+
+def cart(request):
+    return render(request, "cart.html")
+
+def favorites(request):
+    return render(request, "favorites.html")
+
+def profile(request):
+    return render(request, "profile.html")
