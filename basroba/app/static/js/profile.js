d4324858320008c52947d4d87901c326cdaef23a
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-01 15:32:04 +0000

    Sep 1, 2025, 7:32 PM

---
+++ basroba/app/static/js/profile.js
@@ -0,0 +1,21 @@
+const main_part1 = document.getElementById("main-part1");
+const main_part2 = document.getElementById("main-part2");
+const main_part3 = document.getElementById("main-part3");
+
+function open_part1() {
+    main_part1.style.display = "block";
+    main_part2.style.display = "none";
+    main_part3.style.display = "none";
+}
+
+function open_part2() {
+    main_part1.style.display = "none";
+    main_part2.style.display = "block";
+    main_part3.style.display = "none";
+}
+
+function open_part3() {
+    main_part1.style.display = "none";
+    main_part2.style.display = "none";
+    main_part3.style.display = "block";
+}
