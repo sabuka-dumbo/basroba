4a2c907de610b851571ed7ab9256cd465bb0af67
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-05 16:32:49 +0000

    Sep 5, 2025, 8:32 PM

--- basroba/app/urls.py
+++ basroba/app/urls.py
@@ -4,12 +4,8 @@ from django.urls import path
 urlpatterns = [
     path("", views.index, name='index'),
     path("products/", views.products, name="products"),
-    path("product/<int:ID>/", views.product, name="product"),
+    path("product/<int:ID>", views.product, name="product"),
     path("cart/", views.cart, name="cart"),
     path("profile/", views.profile, name="profile"),
     path("favorites/", views.favorites, name="favorites"),
-    
-    # AJAX endpoints
-    path("add-to-favorites/", views.add_to_favorites, name="add_to_favorites"),
-    path("add-to-cart/", views.add_to_cart, name="add_to_cart"),
 ]
