e1f21e90d7e23143ef12718ad092c8652dc91364
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-05 16:40:30 +0000

    Sep 5, 2025, 8:40 PM

--- basroba/app/views.py
+++ basroba/app/views.py
@@ -13,56 +13,52 @@ def products(request):
     return render(request, "products.html")
 
 def product(request, ID):
-    if request.method == "POST":
-        pass
-    else:
-        # Get the product
-        product = get_object_or_404(Product, id=ID)
+    # Get the product
+    product = get_object_or_404(Product, id=ID)
 
-        # Collect all product images
-        product_images = [
-            product.Product_image1,
-            product.Product_image2,
-            product.Product_image3,
-            product.Product_image4,
-            product.Product_image5,
-            product.Product_image6,
-            product.Product_image7,
-            product.Product_image8,
-            product.Product_image9,
-            product.Product_image10,
-        ]
-        # Remove None values
-        product_images = [img for img in product_images if img]
+    # Collect all product images
+    product_images = [
+        product.Product_image1,
+        product.Product_image2,
+        product.Product_image3,
+        product.Product_image4,
+        product.Product_image5,
+        product.Product_image6,
+        product.Product_image7,
+        product.Product_image8,
+        product.Product_image9,
+        product.Product_image10,
+    ]
+    # Remove None values
+    product_images = [img for img in product_images if img]
 
-        # Get colors from ManyToManyField
-        product_colors = product.Product_color.all()
+    # Get colors from ManyToManyField
+    product_colors = product.Product_color.all()
 
-        # Available sizes
-        available_sizes = []
-        size_map = {
-            "XS": product.Product_XS,
-            "S": product.Product_S,
-            "M": product.Product_M,
-            "L": product.Product_L,
-            "XL": product.Product_XL,
-            "2XL": product.Product_2Xl,
-            "3XL": product.Product_3Xl,
-            "4XL": product.Product_4Xl,
-            "5XL": product.Product_5Xl,
-        }
-        for size, stock in size_map.items():
-            if stock > 0:
-                available_sizes.append(size)
+    # Available sizes
+    available_sizes = []
+    size_map = {
+        "XS": product.Product_XS,
+        "S": product.Product_S,
+        "M": product.Product_M,
+        "L": product.Product_L,
+        "XL": product.Product_XL,
+        "2XL": product.Product_2Xl,
+        "3XL": product.Product_3Xl,
+        "4XL": product.Product_4Xl,
+        "5XL": product.Product_5Xl,
+    }
+    for size, stock in size_map.items():
+        if stock > 0:
+            available_sizes.append(size)
 
-        # Render template
-        return render(request, "product.html", {
-            "product": product,
-            "product_images": product_images,
-            "product_colors": product_colors,
-            "available_sizes": available_sizes,
-            "ID": ID
-        })
+    # Render template
+    return render(request, "product.html", {
+        "product": product,
+        "product_images": product_images,
+        "product_colors": product_colors,
+        "available_sizes": available_sizes,
+    })
 
 def cart(request):
     return render(request, "cart.html")
