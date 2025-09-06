fb4f175abde192d1e1d2c72394e90e3d19f8f65c
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-06 14:22:53 +0000

    Sep 6, 2025, 6:22 PM

--- basroba/app/views.py
+++ basroba/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def index(request):
