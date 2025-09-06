df66084e85f7416199cdfe7a676dc6fd63bb258e
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-06 10:16:41 +0000

    Sep 6, 2025, 2:16 PM

--- basroba/app/urls.py
+++ basroba/app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("cart/", views.cart, name="cart"),
     path("profile/", views.profile, name="profile"),
     path("favorites/", views.favorites, name="favorites"),
+
+    # API endpoints
+    path("api/add_to_cart/", views.add_to_cart, name="add_to_cart"),
 ]
