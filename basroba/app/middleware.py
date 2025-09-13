a50d6a6074f598ce15571d93c2455d6e7411d330
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-13 10:29:24 +0000

    Sep 13, 2025, 2:29 PM

---
+++ basroba/app/middleware.py
@@ -0,0 +1,21 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class LanguageRedirectMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ignored_paths = [
+            reverse('choose_language'),
+            reverse('set_language'),
+            '/admin/',
+            '/static/',
+            '/media/',
+        ]
+        if not request.session.get('language_chosen', False):
+            if not any(request.path.startswith(path) for path in ignored_paths):
+                return redirect('choose_language')
+
+        response = self.get_response(request)
+        return response
