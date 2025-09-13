5b7abe2889cb25bd7db0308f487fd6d7a8982bca
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-13 11:21:27 +0000

    Sep 13, 2025, 3:21 PM

--- basroba/basroba/settings.py
+++ basroba/basroba/settings.py
@@ -28,6 +28,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'basroba.urls'
