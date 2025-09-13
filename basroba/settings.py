1db935a2fdec8800487355060944027ad5454da1
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-13 15:42:59 +0400

    s

--- basroba/settings.py
+++ basroba/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-secret-key')
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['your-app-name.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'app',
