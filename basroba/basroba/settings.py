e6f8fcc8ccb6c0c19c35198a9912cf2131fe89cb
Sabuka-Dumbo <anonym.sas@proton.me>
2025-09-13 11:22:30 +0000

    Sep 13, 2025, 3:22 PM

--- basroba/basroba/settings.py
+++ basroba/basroba/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-1k*o!%*&ponqd^ig^l-dbqrm^r3knie_4gc^8jhf59#58k_a=0'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 INSTALLED_APPS = [
     'app',
